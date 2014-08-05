527eecbbd2e94068431618b2db8e5cfaf8ca4934
falconho <endless640c@gmail.com>
2014-08-05 14:31:39 +0800

    no message

--- build/out.php
+++ build/out.php
@@ -2,13 +2,12 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Login</title>
 </head>
 <body>
 <?
 	unset($_SESSION['FBUSER_uid']);
-	echo "登出成功";
-	//echo '<meta http-equiv=REFRESH CONTENT=0;url=bgindex.php>';
+	header('Location: ttp://www.foncon.com.tw/powish/');
+	exit;
 ?>
 
 </body>
--- build/php/register.php
+++ build/php/register.php
@@ -1,46 +1,71 @@
-<?
-session_start();
-include("../lib/mysql.php");
-?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Login</title>
-</head>
-<body>
-<?
-	
-	if($_POST['uid'] != '')
+<?php session_start();
+	include_once("../lib/mysql.php");
+	include_once("../lib/functions.php");
+	if($_POST['email']!="")
 	{
-		$sqlb = "SELECT * FROM fblogin WHERE uid = ".$_POST['uid'];
-		$resultb = exe_sql(DATABASE, $sqlb);
-		
-		if($resultb[0]['uid'] == $_POST['uid'])
+		$address = checkInput($_POST['address']);
+		$cellphone = checkInput($_POST['cellphone']);
+		$fullname = checkInput($_POST['fullname']);
+		$password = checkInput($_POST['password']);
+		$cpassword = checkInput($_POST['cpassword']);
+		$frequency = checkInput($_POST['frequency']);
+		$email = checkInput($_POST['email']);
+
+		if($password==$cpassword)
 		{
-			$_SESSION['FBUSER_uid'] =  $_POST['uid'] ;
-			echo "歡迎回來";
-		}
-		else
-		{
-			$table = "fblogin";
-			$data_array['uid'] = $_POST['uid'];
-			$data_array['name'] = $_POST['name'];
-			$data_array['first_name'] = $_POST['first_name'];
-			$data_array['last_name'] = $_POST['last_name'];
-			$data_array['email'] = $_POST['email'];
-			$data_array['gender'] = $_POST['gender'];
-			$data_array['link'] = $_POST['link'];
-			$data_array['locale'] = $_POST['locale'];
-			$data_array['timezone'] = $_POST['timezone'];
-			$data_array['updated_time'] = $_POST['updated_time'];
-			insert(DATABASE, $table, $data_array);
-			$_SESSION['FBUSER_uid'] =  $_POST['uid'] ;
-			echo "註冊成功!!";
+			$password = sha1($password);
+
+			$sql = "SELECT * FROM member WHERE account = '$email'";
+			$result = exe_sql(DATABASE, $sql);
+			if($result[0]['account'] != $email )
+			{
+				$data_array['account'] =  $email;
+				$data_array['password'] = $password;
+				$data_array['fullname'] = $fullname;
+				$data_array['card'] = '';
+				$data_array['cellphone'] = $cellphone;
+				$data_array['address'] = $address;
+				$data_array['brand'] = 0;
+				$data_array['method'] = 0;
+				$data_array['verification'] = sha1($data_array['account'].$data_array['deadline']);
+				$data_array['deadline'] = (time()+(24 * 60 * 60));
+				$data_array['verify_status'] = 0;
+				$data_array['level'] = 0;
+				$table="member";
+				if(insert(DATABASE, $table, $data_array)){
+					//header("HTTP/1.1 301 Moved Permanently");
+					//header("Location: membership.php");
+					echo "OK";
+					$_SESSION['account'] = $email;
+					getMemberData($_SESSION['account']);
+					if(count($_POST["cartId"])>0)
+					{
+						$key_column = "MID";
+						$id = $_SESSION['mid'];
+						$table ="cart";
+						$sql_array['STATUS'] = 1;
+						update(DATABASE, $table, $sql_array, $key_column, $id);
+						//VOID CART
+
+						for($i=0;$i<count($_POST["cartId"]);$i++)
+						{
+							$data_array['MID'] = $_SESSION['mid'];
+							$data_array['PID'] = $_POST["cartId"][$i];
+							$data_array['QTY'] = $_POST["number"][$i];
+							$data_array['AMOUNT'] = $_POST["price"][$i];
+							$table = "cart";
+							insert(DATABASE, $table, $data_array);
+						}
+					}
+				}
+			}else{
+				echo "此信箱已註冊，請查詢密碼！";
+			}
+		}else{
+				echo "你輸入兩次的密碼不一樣！";
 		}
 	}else{
-		echo "發生不能預期";
+		echo "no data";
 	}
-?>
 
-</body>
-</html>
+?>
